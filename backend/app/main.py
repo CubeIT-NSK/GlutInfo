121d067ae86306eeea4e35a86ffdeac96c0ebd57
pusha <ptyrtin@mail.ru>
2024-11-12 02:13:38 +0700

    add backend docker compose

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,10 @@ from app.core.config import settings
 from app.admin.core import create_admin_core
 
 
-app = FastAPI(title=settings.app_title)
+app = FastAPI(
+    title=settings.app_title,
+    openapi_url="/docs/openapi.json",
+)
 admin = create_admin_core(app)
 
 app.include_router(main_router)
