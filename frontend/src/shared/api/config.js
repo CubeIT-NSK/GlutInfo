66029d9c08d7850cdea97b360bbb92e9c2b8e9a2
pusha <ptyrtin@mail.ru>
2024-11-24 01:01:47 +0700

    change config

--- frontend/src/shared/api/config.js
+++ frontend/src/shared/api/config.js
@@ -1,5 +1,5 @@
-// export const mainDomain = "http://89.111.169.68"; // Can change to var from .env
-export const mainDomain = "http://localhost:8000/"; // Can change to var from .env
+export const mainDomain = "http://89.111.169.68"; // Can change to var from .env
+// export const mainDomain = "http://localhost:8000/"; // Can change to var from .env
 export const apiVersion = "/api/v1"; // Can change to var from .env
 
 export const apiUrl = mainDomain + apiVersion
